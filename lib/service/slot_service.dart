42f535b9544fbf50751c509c3b145a48917c3202
Aristocrat <crataristo4@gmail.com>
2021-09-03 17:50:30 +0000

    bug fix

--- lib/service/slot_service.dart
+++ lib/service/slot_service.dart
@@ -1,33 +1,26 @@
 import 'package:astrologyapp/constants/constants.dart';
 import 'package:astrologyapp/model/slot.dart';
+import 'package:astrologyapp/pages/AccountPage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class SlotService {
   final firestoreService = FirebaseFirestore.instance;
   static SlotService? _instance;
-  static String? email;
 
   SlotService._();
 
   static SlotService get instance {
-    if (FirebaseAuth.instance.currentUser != null) {
-      email = FirebaseAuth.instance.currentUser!.email;
-    }
-
     return _instance == null ? _instance = SlotService._() : _instance!;
   }
 
 //create new slot
   Future<void> createNewSlot(Slots slot) async {
-    if (email != null) {
-      return await firestoreService
-          .collection(astrologerX)
-          .doc(email)
-          .collection(slots)
-          .doc(slot.day)
-          .set(slot.toJson());
-    }
+    return await firestoreService
+        .collection(astrologerX)
+        .doc(email)
+        .collection(slots)
+        .doc(slot.day)
+        .set(slot.toJson());
   }
 
   //fetch slots
