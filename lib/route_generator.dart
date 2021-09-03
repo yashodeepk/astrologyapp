2290fec0ee286409ca968b7e2c57b5594ec03fdd
Aristocrat <crataristo4@gmail.com>
2021-09-03 14:27:52 +0000

    routing added

---
+++ lib/route_generator.dart
@@ -0,0 +1,39 @@
+import 'package:astrologyapp/pages/AccountPage.dart';
+import 'package:astrologyapp/pages/HomePage.dart';
+import 'package:astrologyapp/pages/schedules_page/schedules.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case Home.routeName:
+        return MaterialPageRoute(builder: (_) => Home());
+
+      case AccountPageWidget.routeName:
+        return MaterialPageRoute(builder: (_) => AccountPageWidget());
+
+      case HomePageWidget.routeName:
+        return MaterialPageRoute(builder: (_) => HomePageWidget());
+
+      case SchedulesPage.routeName:
+        return MaterialPageRoute(builder: (_) => SchedulesPage());
+      default:
+        return _errorRoute();
+    }
+  }
+
+  //error page ..
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        body: Center(
+          child: Text("Page not Found"),
+        ),
+      );
+    });
+  }
+}
