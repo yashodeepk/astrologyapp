cfa4cd9ebd72648b60f22e5ef479d1d3fb7b2b5f
Aristocrat <crataristo4@gmail.com>
2021-09-11 08:06:49 +0000

    added payment api

---
+++ lib/api/payment_api.dart
@@ -0,0 +1,66 @@
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+class PaymentApi {
+  PaymentApi._();
+
+  static PaymentApi? _instance;
+  static Razorpay? _razorpay;
+  PaymentState paymentState = PaymentState.INITIAL;
+
+  static PaymentApi get instance {
+    return _instance == null ? _instance = PaymentApi._() : _instance!;
+  }
+
+  void initializeRazorPay() {
+    _razorpay = Razorpay();
+    _razorpay!.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay!.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay!.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
+
+  void launchRazorPay(
+      int amount, String name, String description, String email) {
+    amount = amount * 100;
+
+    var options = {
+      'key': 'rzp_test_F4IrIZIbK0GW0Y',
+      'amount': "$amount",
+      'name': name,
+      'description': description,
+      'prefill': {'contact': "", 'email': email}
+    };
+
+    try {
+      _razorpay!.open(options);
+
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+
+    if(response.orderId != null ){
+      paymentState = PaymentState.SUCCESS;
+      print(" success");
+      print(
+          "${response.orderId} \n${response.paymentId} \n${response.signature}");
+    }
+
+
+
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    paymentState = PaymentState.FAILURE;
+    print("Payment Failed");
+
+    print("${response.code}\n${response.message}");
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    print("Payment Failed");
+  }
+}
+
+enum PaymentState { INITIAL, SUCCESS, FAILURE }
