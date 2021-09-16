15c1167ad3a72eae439db6872ee6cac97c6f1e0b
Aristocrat <crataristo4@gmail.com>
2021-09-16 13:05:17 +0000

    added progress_dialog.dart

---
+++ lib/actions/progress_dialog.dart
@@ -0,0 +1,39 @@
+import 'package:astrologyapp/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+class Dialogs {
+  static Future<void> showLoadingDialog(
+      BuildContext context, GlobalKey key, String title, Color bgColor) async {
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return WillPopScope(
+              onWillPop: () async => true,
+              child: SimpleDialog(
+                  key: key,
+                  backgroundColor: bgColor,
+                  children: <Widget>[
+                    Center(
+                      child: Column(children: [
+                        Text(title,
+                            style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontWeight: FontWeight.w700)),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        CircularProgressIndicator(),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          pleaseWait,
+                          style: TextStyle(color: Colors.blueAccent),
+                        )
+                      ]),
+                    )
+                  ]));
+        });
+  }
+}
