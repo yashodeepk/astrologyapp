08f2a5dc22110686ffd301eb1df9bcb380b1fb59
Yashodeep Kacholiya <yashodeepkacholiya@Yashodeeps-MacBook-Air.local>
2021-10-01 16:46:55 +0530

    ios project fix

--- lib/GoogleMeetUtils/secrate.dart
+++ lib/GoogleMeetUtils/secrate.dart
@@ -4,7 +4,7 @@ class Secret {
   static const ANDROID_CLIENT_ID =
       "121239416780-kan19lh4caieskosdm6pkhlh9pba9dou.apps.googleusercontent.com";
   static const IOS_CLIENT_ID =
-      "";
+      "121239416780-1a0cp9jgd2jj7mfeq45eebjs71d24vuk.apps.googleusercontent.com";
   static String getId() =>
       Platform.isAndroid ? Secret.ANDROID_CLIENT_ID : Secret.IOS_CLIENT_ID;
   //static String getId() => Secret.ANDROID_CLIENT_ID;
